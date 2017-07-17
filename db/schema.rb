059a05dc20489d71504c6ff110e0dffe5452b92b
Allen Zhong <zhong.allen@gmail.com>
2017-07-18 00:43:18 +1200

    execute migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712035339) do
+ActiveRecord::Schema.define(version: 20170717100125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170712035339) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "banner"
+    t.string "banner_link"
   end
 
 end
