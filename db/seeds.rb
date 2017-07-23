051cc433fdb092bcf554efa550cbcbb5c04aa003
Allen Zhong <zhong.allen@gmail.com>
2017-07-23 15:21:00 +1200

    fix seeds problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 jose = Signature.create({
   name: "José-Marie CORTÈS",
   position: "Director - Representative                      of the Fondation Alliance Française   in New Zealand & Cook Islands",
-  telephone: "+64(09)-376 00009",
+  telphone: "+64(09)-376 00009",
   email: "director@alliance-francaise.co.nz",
   street: "9a Kirk Street, Grey Lynn",
   city: "Auckland",
@@ -16,5 +16,6 @@ jose = Signature.create({
   country: "New Zealand",
   website: "www.alliance-francaise.co.nz",
   banner: "https://alliance-francaise.sslsvc.com/media/website_posts/736/winter-intensive-beginners-website-banner2.png",
-  bannerLink: "http://www.alliance-francaise.co.nz/learn-french/intensive-immersion/?age_group_id=1&location_id=1&session_id=77&type_id=8&clicked_link=1&level_id=102"
+  banner_link: "http://www.alliance-francaise.co.nz/learn-french/intensive-immersion/?age_group_id=1&location_id=1&session_id=77&type_id=8&clicked_link=1&level_id=102"
 })
+
