f400f468db762dee9882eb1a37c5e285e4d61f83
Allen Zhong <zhong.allen@gmail.com>
2017-07-23 15:21:11 +1200

    add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'signatures#index'
   get 'signatures/index'
-
+  resources :signatures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
